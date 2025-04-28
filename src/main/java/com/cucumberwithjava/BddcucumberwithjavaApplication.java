a1498402ab3a9397c0ab5068fa9c23880a9f0eab
olaminiyi <olaiyaolaminiyi@gmail.com>
2025-04-28 23:41:46 +0100

    first commit

---
+++ src/main/java/com/cucumberwithjava/BddcucumberwithjavaApplication.java
@@ -0,0 +1,13 @@
+package com.cucumberwithjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BddcucumberwithjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BddcucumberwithjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cucumberwithjava/BddcucumberwithjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cucumberwithjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BddcucumberwithjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
